67a3eed4f73aa1aaa9580c3dbd3ca83231568fc5
Cuprizevich <vlad20553@ukr.net>
2016-09-14 15:37:23 +0300

    PARSER

--- main.cpp
+++ main.cpp
@@ -11,23 +11,14 @@ static char * ReadAllBytes(const char * filename, int * read)
     ifstream ifs(filename, ios::binary|ios::ate);
     ifstream::pos_type pos = ifs.tellg();
 
-    // What happens if the OS supports really big files.
-    // It may be larger than 32 bits?
-    // This will silently truncate the value/
     int length = pos;
 
-    // Manuall memory management.
-    // Not a good idea use a container/.
     try{
         char *pChars = new char[length];
 
         ifs.seekg(0, ios::beg);
         ifs.read(pChars, length);
 
-        // No need to manually close.
-        // When the stream goes out of scope it will close the file
-        // automatically. Unless you are checking the close for errors
-        // let the destructor do it.
         ifs.close();
         *read = length;
         return pChars;
@@ -36,14 +27,45 @@ static char * ReadAllBytes(const char * filename, int * read)
     }
 }
 
-int main() {
-    string fileName = "D:\\LAB\\Projects\\GitGub\\acyclic-graph-from-json\\data.json";
+vector<node> parseJson(const char* fileName){
     int read;
-    char * json = ReadAllBytes(fileName.c_str(), &read);
+    char * json = ReadAllBytes(fileName, &read);
 
     Document document;
     document.Parse(json);
 
+    const Value& myNodes = document["nodes"];
+    vector<node> resNodes(myNodes.Size());
+
+    for(int i = 0; i < myNodes.Size(); i++){
+        vector<mark> resMarks(myNodes[i]["marks"].Size());
+
+        for(int j = 0; j < myNodes[i]["marks"].Size(); j++){
+
+            const Value& myMark = myNodes[i]["marks"][j];
+            vector <vector<int>> resLinks(myNodes.Size());
+
+            for(int x =0; x < myNodes.Size(); x++) resLinks[x].resize(myMark["links"].Size());
+
+            for(int x = 0; x < myMark["links"].Size(); x++){
+                const Value& myLink = myMark["links"][x];
+
+                for(int y = 0; y < myMark["links"].Size(); y++){
+                    resLinks[myLink["toNode"].GetInt()-1][myLink["toMark"].GetInt()-1] = myLink["weight"].GetInt();
+                }
+            }
+            resMarks[j].setmark(j,myMark["weight"].GetInt(),myNodes.Size(),resLinks);
+        }
+        resNodes[i].setnode(i,resMarks);
+    }
+
+    return resNodes;
+}
+
+int main() {
+    string fileName = "D:\\LAB\\Projects\\GitGub\\acyclic-graph-from-json\\data.json";
+
+    vector<node> myNodes = parseJson(fileName.c_str());
     /*
     const Value& a = document["nodes"];
     const Value& b = a[0];
@@ -52,17 +74,7 @@ int main() {
      */
     //MembersCount() - returns count of fields
 
-    int nodesCount = document["nodes"].Size();
-    const Value& nodes = document["nodes"];
-
-    vector<node> myNodes;
-    myNodes.resize(nodesCount);
-
-    for(int i = 0; i < nodesCount; i++){
-        const Value& myNode = nodes[i];
-        const Value& marks = myNode["marks"];
-
-    }
+    cout<<myNodes[1].marks[0].links[3][0];
 
     return 0;
 }
--- node.h
+++ node.h
@@ -10,21 +10,39 @@
 using namespace std;
 
 class mark{
+public:
+    int id;
     int weight;
+    int nodesCount;
     vector <vector<int>> links;
+
+    void setmark(int _id, int _weight, int _nodesCount, vector<vector<int>> _links){
+
+        id = _id;
+        weight = _weight;
+        nodesCount  = _nodesCount;
+        links.resize(_links.size());
+        for(int i = 0; i < _links.size(); i++)
+            links[i].resize(_links[i].size());
+        links = _links;
+    }
 };
 
 class node {
-private:
+public:
     int id;
     vector <mark> marks;
     //vector <int> neighbours;
 
-public:
-    node(vector<mark> _marks){
+
+    void setnode(int _id, vector<mark> _marks){
+        id = _id;
         marks.resize(_marks.size());
         marks = _marks;
     }
+
+
+
 };
 
 
